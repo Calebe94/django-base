863818fbb85d5c8a5673a641005757ad0c7e9469
Calebe94 <calebe94@disroot.org>
2023-09-17 19:13:06 -0300

    Added changes from update and create class

--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 from rest_framework.response import Response
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
@@ -15,8 +16,19 @@ class MovieViewSet(viewsets.ModelViewSet):
         status_code = status.HTTP_200_OK
         if 'stars' in request.data:
             movie = Movie.objects.get(id=pk)
-            print(movie.title)
-            response = {'message': "{}".format(movie.title)}
+            stars = request.data['stars']
+            # user = request.user
+            user = User.objects.get(id=1)
+            try:
+                rating = Rating.objects.get(user=user.id, movie=movie.id)
+                rating.stars = stars
+                rating.save()
+                serializer = RatingSerializer(rating, many=False)
+                response = {'message': "Rating updated: {}".format(serializer.data)}
+            except:
+                rating = Rating.objects.create(user=user, movie=movie, stars=stars)
+                serializer = RatingSerializer(rating, many=False)
+                response = {'message': "Rating created: {}".format(serializer.data)}
         else:
             status_code = status.HTTP_400_BAD_REQUEST
             response = {'message': "you need to provide stars"}
